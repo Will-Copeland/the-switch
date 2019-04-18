3bb8ac2d2bc4b0a1220ba7d35ad26d5dba35df2a
Will-Copeland <will@localhost.localdomain>
2019-04-17 20:40:26 -0700

    pruning unused queries

--- server/index.js
+++ server/index.js
@@ -3,58 +3,47 @@ const { GraphQLServer } = require('graphql-yoga');
 
 const resolvers = {
   Query: {
-    publishedPosts(root, args, context) {
-      return context.prisma.posts({ where: { published: true } });
-    },
-    post(root, args, context) {
-      return context.prisma.post({ id: args.postId });
-    },
-    postsByUser(root, args, context) {
-      return context.prisma.user({
-        id: args.userId,
-      }).posts();
+    // publishedPosts(root, args, context) {
+    //   return context.prisma.posts({ where: { published: true } });
+    // },
+    // post(root, args, context) {
+    //   return context.prisma.post({ id: args.postId });
+    // },
+    // postsByUser(root, args, context) {
+    //   return context.prisma.user({
+    //     id: args.userId,
+    //   }).posts();
+    // },
+    getSwitchState(root, args, context) {
+      return context.prisma.switchEvents({ last: 1 });
     },
   },
   Mutation: {
-    createDraft(root, args, context) {
-      return context.prisma.createPost(
-        {
-          title: args.title,
-          author: {
-            connect: { id: args.userId },
-          },
-        },
+  //   createDraft(root, args, context) {
+  //     return context.prisma.createPost(
+  //       {
+  //         title: args.title,
+  //         author: {
+  //           connect: { id: args.userId },
+  //         },
+  //       },
 
-      );
-    },
-    publish(root, args, context) {
-      return context.prisma.updatePost(
-        {
-          where: { id: args.postId },
-          data: { published: true },
-        },
+  //     );
+  //   },
+  //   publish(root, args, context) {
+  //     return context.prisma.updatePost(
+  //       {
+  //         where: { id: args.postId },
+  //         data: { published: true },
+  //       },
 
-      );
-    },
-    createUser(root, args, context) {
-      return context.prisma.createUser(
-        { name: args.name },
-      );
-    },
-  },
-  User: {
-    posts(root, args, context) {
-      return context.prisma.user({
-        id: root.id,
-      }).posts();
-    },
-  },
-  Post: {
-    author(root, args, context) {
-      return context.prisma.post({
-        id: root.id,
-      }).author();
-    },
+  //     );
+  //   },
+  //   createSwitchEvent(root, args, context) {
+  //     return context.prisma.createSwitchEvent(
+  //       { name: args.name },
+  //     );
+  //   },
   },
 };
 
